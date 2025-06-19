e7e5fd5a3dd669eee72c13b7cacb94bff6448123
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-06-19 19:36:44 +0530

    changed codes like html and image croper

--- image croper.py
+++ image croper.py
@@ -1,5 +1,5 @@
 from PIL import Image
 
-img = Image.open("original_digit.png").convert("L")
+img = Image.open("frontend/originalimg.jpg").convert("L")
 img = img.resize((28, 28))
 img.save("resized_digit.png")
